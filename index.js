b6bf36f277998e5791b020091fbb58d79bb30088
cschmitz <schmitz.chris@gmail.com>
2016-02-23 14:45:55 -0600

    Adds encouragement

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = [
     "don't worry. no one actually knows what they're doing."
   , "this is Hard Stuff, but you can do it!"
+  , "If you can see this message it means you got it working and that's something to celebrate! ['hip','hip']!"
 ];

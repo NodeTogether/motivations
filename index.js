c3a6e3099c110e06af4deb826be8253c159f2249
Charlotte Spencer <charlottelaspencer@gmail.com>
2016-02-23 20:45:49 +0000

    add part motivational, part nonsensical, quotes

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ module.exports = [
     "don't worry. no one actually knows what they're doing."
   , "this is Hard Stuff, but you can do it!"
   , "If you can see this message it means you got it working and that's something to celebrate! ['hip','hip']!"
+  , "you are the best around"
+  , "computers are super hard, but you can defeat them!"
+  , "it's okay to need a break, everyone needs one!"
+  , "you got this"
+  , "whoah you're pretty good at this"
+  , "all the cats in the land bow down to your cool computer skills"
 ];

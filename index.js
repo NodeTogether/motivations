8158d6a0b2917d23d53c8c5c66470dc0823de7ef
Michael Mazurczak <contact@maz-dev.cc>
2016-02-23 21:56:22 +0100

    Added you did it

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = [
     "don't worry. no one actually knows what they're doing."
   , "this is Hard Stuff, but you can do it!"
-  , "getting started is hard, congratulations!"
+  , "getting started is hard, you did it, congratulations!"
 ];

a2cb558b22528fda7dbd50f5288fb883486898cb
Julian Duque <julianduquej@gmail.com>
2016-02-25 00:13:09 -0500

    Add a couple of motivations, one in spanish :)

--- index.js
+++ index.js
@@ -24,4 +24,6 @@ module.exports = [
   , "I'm pretty sure you're now fully qualified fantastic human."
   , "looks like you're pretty much awesome now."
   , "omg - I'm falling head over heels for your sweet skills."
+  , "the best way to learn is by doing, and you are doing it good!"
+  , "¡Lo estás haciendo muy bien!"
 ];

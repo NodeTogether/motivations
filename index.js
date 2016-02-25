25f7fdb50fb1822fd6882a2ea99e02a26fd3b460
Kent C. Dodds <kent+github@doddsfamily.us>
2016-02-24 23:19:47 -0700

    motivate

--- index.js
+++ index.js
@@ -24,4 +24,6 @@ module.exports = [
   , "I'm pretty sure you're now fully qualified fantastic human."
   , "looks like you're pretty much awesome now."
   , "omg - I'm falling head over heels for your sweet skills."
+  , "keep going! you can do it!"
+  , "mistakes are how we learn. keep making them! you're doing awesome!"
 ];

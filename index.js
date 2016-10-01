88c70ce5e1e87e05fdcafc04f55c777e7ac78001
Ashley Williams <ashley666ashley@gmail.com>
2016-10-01 13:57:52 -0400

    fix bad merge

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ module.exports = [
   , "\✲ﾟ｡\.\(✿╹◡╹\)ﾉ☆\.｡₀\:\*ﾟ\✲ﾟ\*\:₀｡wow i cant believe how awesome this is"
   , "\"No bird soars in a calm\" - Wilbur Wright"
   , "Ayyyyyyy! so much WoW! very aWesome!!!"
-<<<<<<< HEAD
   , "you finished a hard problem and learned something new. doesn't it feel great?"
   , "\"Nothing is impossible, the word itself says \'I\'m possible!\'\" - Audrey Hepburn"
   , "Never leave house without your towel."
